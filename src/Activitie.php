dbee0399a8c335c3ecbc43eaff28b9df73c600a8
vanhaohero1998 <quachvanhao2020@gmail.com>
2020-11-06 10:31:25 +0700

    new

--- src/Activitie.php
+++ src/Activitie.php
@@ -3,44 +3,106 @@ namespace Ecomo;
 use Identimo\User;
 use DateTime;
 use YPHP\Entity;
+use YPHP\EntityFertility;
+use Identimo\UserInterface;
+use YPHP\EntityInterface;
+use YPHP\EntityLife;
 
-class Activitie extends Entity{
+class Activitie extends EntityLife{
+    const OWNER = "owner";
+    const TARGER = "target";
+
+    public function __toArray(){
+        return array_merge(parent::__toArray(),[
+            self::OWNER => $this->getOwner(),
+            self::TARGER => $this->getTarget(),
+        ]);
+    }
+
     /**
-    * @var int
+    * @var UserInterface
     */
-    public $amount = 0 ;
-    /**
-    * @var string
+    protected $owner;
+
+        /**
+    * @var EntityInterface
     */
-    public $composedId = "string" ;
-    /**
-    * @var DateTime
-    */
-    public $date = null ;
-            /**
-    * @var string
-    */
-    public $email = "string" ;
-    public $emailType = "OrderEventsEmailsEnum" ;
+    protected $target;
+
         /**
     * @var string
     */
-    public $message = "string" ;
-            /**
-    * @var string
-    */
-    public $order0 = "string" ;
+    protected $type;
+
     /**
-    * @var string[]
-    */
-    public $oversoldItems = ("string" );
-        /**
-    * @var int
-    */
-    public $quantity = 0 ;
-    public $type = "OrderEventsEnum" ;
+     * Get the value of owner
+     *
+     * @return  UserInterface
+     */ 
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
     /**
-    * @var User
-    */
-    public $user;
+     * Set the value of owner
+     *
+     * @param  UserInterface  $owner
+     *
+     * @return  self
+     */ 
+    public function setOwner(UserInterface $owner = null)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     *
+     * @return  string
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @param  string  $type
+     *
+     * @return  self
+     */ 
+    public function setType(string $type = null)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of target
+     *
+     * @return  EntityInterface
+     */ 
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * Set the value of target
+     *
+     * @param  EntityInterface  $target
+     *
+     * @return  self
+     */ 
+    public function setTarget(EntityInterface $target = null)
+    {
+        $this->target = $target;
+
+        return $this;
+    }
 }
