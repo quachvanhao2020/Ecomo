a538bfa301da618dcd472e0bd333ae0469b68a36
vanhaohero1998 <quachvanhao2020@gmail.com>
2020-11-06 19:27:44 +0700

    new

--- src/HomeEcomo.php
+++ src/HomeEcomo.php
@@ -1,25 +1,138 @@
 <?php
 namespace Ecomo;
 use Ecomo\Storage\ActivitieStorage;
-use Ecomo\Orders\Storage\OrderStorage;
-use Ecomo\Products\Product;
-use Ecomo\Money;
+use Ecomo\Orders\OrderStatistical;
+use Ecomo\Products\ProductStatistical;
+use Identimo\Statistical\StaffStatistical;
+use YPHP\Entity;
+use YPHP\EntityLife;
 
-class HomeEcomo {
+class HomeEcomo extends Entity{
+
+    const ORDERSTATISTICAL = "orderStatistical";
+    const PRODUCTSTATISTICAL = "productStatistical";
+    const STAFFSTATISTICAL = "staffStatistical";
+    const ACTIVITIES = "activities";
+
+    public function __toArray(){
+      return array_merge([
+          self::ORDERSTATISTICAL => $this->getOrderStatistical(),
+          self::PRODUCTSTATISTICAL => $this->getProductStatistical(),
+          self::STAFFSTATISTICAL => $this->getStaffStatistical(),
+          self::ACTIVITIES => $this->getActivities(),
+      ],parent::__toArray());
+  }
+
       /**
-    * @var Money
+    * @var OrderStatistical
     */
-    public $sales;
-      /**
-    * @var OrderStorage
-    */
-    public $orders;
+    public $orderStatistical;
     /**
-    * @var Product
+    * @var ProductStatistical
     */
-    public $product;
+    public $productStatistical;
+    /**
+    * @var StaffStatistical
+    */
+    public $staffStatistical;
     /**
     * @var ActivitieStorage
     */
     public $activities;
+
+    /**
+     * Get the value of orderStatistical
+     *
+     * @return  OrderStatistical
+     */ 
+    public function getOrderStatistical()
+    {
+        return $this->orderStatistical;
+    }
+
+    /**
+     * Set the value of orderStatistical
+     *
+     * @param  OrderStatistical  $orderStatistical
+     *
+     * @return  self
+     */ 
+    public function setOrderStatistical(?OrderStatistical $orderStatistical)
+    {
+        $this->orderStatistical = $orderStatistical;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productStatistical
+     *
+     * @return  ProductStatistical
+     */ 
+    public function getProductStatistical()
+    {
+        return $this->productStatistical;
+    }
+
+    /**
+     * Set the value of productStatistical
+     *
+     * @param  ProductStatistical  $productStatistical
+     *
+     * @return  self
+     */ 
+    public function setProductStatistical(?ProductStatistical $productStatistical)
+    {
+        $this->productStatistical = $productStatistical;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of staffStatistical
+     *
+     * @return  StaffStatistical
+     */ 
+    public function getStaffStatistical()
+    {
+        return $this->staffStatistical;
+    }
+
+    /**
+     * Set the value of staffStatistical
+     *
+     * @param  StaffStatistical  $staffStatistical
+     *
+     * @return  self
+     */ 
+    public function setStaffStatistical(?StaffStatistical $staffStatistical)
+    {
+        $this->staffStatistical = $staffStatistical;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of activities
+     *
+     * @return  ActivitieStorage
+     */ 
+    public function getActivities()
+    {
+        return $this->activities;
+    }
+
+    /**
+     * Set the value of activities
+     *
+     * @param  ActivitieStorage  $activities
+     *
+     * @return  self
+     */ 
+    public function setActivities(?ActivitieStorage $activities)
+    {
+        $this->activities = $activities;
+
+        return $this;
+    }
 }
